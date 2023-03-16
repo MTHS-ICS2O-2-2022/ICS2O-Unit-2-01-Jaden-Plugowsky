ad096a5cde7b4698f3eb1668093dde512cc6464f
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-03-16 15:25:27 +0000

    template copied

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jaden Plugowsky
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+function buttonOneClicked() {
+  alert("Hello, World!")
+}
